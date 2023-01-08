aaf311d1b5e32ae04479fd5ddb15ecff87e65bd9
oseni ade <oseniadedoyin22@gmail.com>
2023-01-08 02:22:39 +0100

    wip

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes/routes");
+var connect = require("connect");
 
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
 const mongoUrl = process.env.MONGO_URI;
 
 const database = mongoose.connection;
@@ -24,6 +24,6 @@ app.use(express.json());
 
 app.use("/api", routes);
 
-app.listen(port, () => {
-  console.log("listening on port " + port);
+app.listen(5000, () => {
+  console.log("listening on port " + 5000);
 });
